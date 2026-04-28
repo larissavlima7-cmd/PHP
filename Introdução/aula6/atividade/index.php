bcdfa9ff5398ba275029f32bd59e9f05d619d6ca
lvlima <larissa.v.lima7@aluno.senai.br>
2026-04-28 13:20:31 -0300

    alteração

--- Introdução/aula6/atividade/index.php
+++ Introdução/aula6/atividade/index.php
@@ -13,7 +13,8 @@
     //while para o controle de vendas
         while($Vendas<$MetaVenda){
             $AtingirMeta=$MetaVenda-$Vendas;
-            echo "Essa é a venda de número " . $Vendas . ", faltam apenas " . $AtingirMeta . " para atingir a meta.";
+            echo "Essa é a venda de número " . $Vendas . ", faltam apenas " . $AtingirMeta . " para atingir a meta. <br>";
+            $Vendas++;
         }
     //if para controle de meta atingida
         if($Vendas>=25){
