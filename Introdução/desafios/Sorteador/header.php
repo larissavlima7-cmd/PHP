35bcee43f26f429a04336687ee306fac7726a666
lvlima <larissa.v.lima7@aluno.senai.br>
2026-05-14 10:24:55 -0300

    sortedor

---
+++ Introdução/desafios/Sorteador/header.php
@@ -0,0 +1,2 @@
+<h1>Sorteador de Número🎰 </h1>
+<p>Este sorteador, escolhe um número aleatoriamente entre 1 e 60, e depois exibe no formato da Mega Sena.</p>
--- Introdução/desafios/Sorteador/index.php
+++ Introdução/desafios/Sorteador/index.php
@@ -1,1 +1,45 @@
-//mt_rand().
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorteador de Número</title>
+    <style>
+        <?php
+            require "style.css";
+        ?>
+    </style>
+</head>
+<body>
+    <?php
+        include "header.php";
+    ?>
+    <form>
+        <button type="submit" class="btn">Sortear</button>
+    </form>
+    <div id="resp">
+    <?php
+    // Variaveis
+    $quantSortear=6;//quantidade que precisa ser sorteado
+    $quantsorteados=0;//quantidade que foi sorteada
+    $sorteados=[];//aray para guardar os seis números sorteados
+
+    // Usando o while, para que enquanto a quantidade que foi sorteada não atingir a quantodade que precisa ser sorteado
+    while($quantsorteados<$quantSortear){
+        //comando para realizar o sorteio
+        $sorteio=mt_rand(1,60);
+        //adiciona os números sorteados ao array
+        if(!in_array($sorteio, $sorteados)){
+            $sorteados[]=$sorteio;
+        }
+        //conforme for rodando, vai aumentando a quantidade de números que foi sorteado
+        $quantsorteados++;
+    }
+    // Saida, visualização dos números
+        echo "<strong>Números Sorteados:</strong> <br>";
+        echo implode (" - ", $sorteados);
+    ?>
+    </div>
+    
+</body>
+</html>
